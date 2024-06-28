5b1f353d0aec323d2a75d7766c452d43d892f930
JaySingh <jaysingh1452@gmail.com>
2024-06-28 23:02:58 +0530

    fixed navbar errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-
+import logo from '../assets/images/Logo.png';
+import profile from '../assets/images/Profile.png';
 function Navbar() {
   return (
     <div className="md:w-1/5 w-full order-1 flex md:flex-col justify-between md:p-4 p-2 bg-blacked">
         <div>
-            <img src="../src/assets/images/Logo.png" 
+            <img src={logo} 
                  alt="spotify-logo" 
                  className='w-30 h-30'/>
         </div>
         <div>
-            <img src="../src/assets/images/Profile.png" 
+            <img src={profile} 
                   alt="user-image" 
                   className='h-10 w-10 rounded-full'/>
         </div>
